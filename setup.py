62d1b6d6d0e4c211b6952a723ec2fae401d6c3a9
Zihao Wang <zhwang000@gmail.com>
2023-01-23 18:12:10 -0500

    Add config folder

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='qtrlb',
       version='0.1.0',
       description='Quantum Control using Qblox',
-      url='http://github.com/',
+      url='https://github.com/Zihao96/qtrlb.git',
       author='Zihao Wang, Rayleigh Parker, Machiel Blok',
       author_email='zwang156@ur.rochester.edu',
       #license='MIT',
