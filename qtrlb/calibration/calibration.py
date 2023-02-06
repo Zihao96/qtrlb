5eb23d55b45f8271bb8a42e356509e671dbaf2fc
Zihao Wang <zhwang000@gmail.com>
2023-02-05 23:18:36 -0500

    Structrue seq_prog

--- qtrlb/calibration/calibration.py
+++ qtrlb/calibration/calibration.py
@@ -1,5 +1,7 @@
 import numpy as np
-
+import pandas as pd
+from qtrlb.utils.waveforms import get_waveform  # TODO: Write it.
+from qtrlb.utils.pulse import pulse_interpreter  # TODO: Write it. Make sure Empty DataFrame works.
 
 
 
@@ -47,6 +49,8 @@ class Scan:
         self.postpulse = postpulse if postpulse is not None else {}
         self.fitmodel = fitmodel
         
+        self.qudits = self.drive_qubits + self.readout_resonators
+        self.heralding_enable = self.config.varman['commmon/heralding']
         self.initialize()
         
         
@@ -67,7 +71,7 @@ class Scan:
         """
         Check and reshape the input parameters.
         Configure the Qblox based on drive_qubits and readout_resonators using in this scan.
-        Generate self.system_info.
+        Generate self.full_prepulse_df.
         Add subspace prepulse and pad the prepulse/postpulse with 'I'.
         We call implement_parameters methods here instead of during init/load of DACManager,
         because we want those modules/sequencers not being used to keep their default status.
@@ -79,16 +83,7 @@ class Scan:
                                              resonators=self.readout_resonators,
                                              subspace=self.subspace)
         
-        self.system_info = {}
-        for qudit in (self.drive_qubits + self.readout_resonators):
-            self.system_info[qudit] = {}
-            # self.system_info[qudit]['sequence_dict'] = {}
-            self.system_info[qudit]['prepulse'] = []
-            self.system_info[qudit]['postpulse'] = []
-        
-        # TODO: Or maybe move it into Sequence.
-        self.pad_subspace_prepulse_postpulse()
-        
+        self.generate_prepulse_postpulse_dataframe()        
         
         
     def check_attribute(self):
@@ -98,10 +93,10 @@ class Scan:
         Make sure each qubit has a scan_start, scan_stop, subspace.
         Make sure the prepulse/postpulse is indeed in form of dictionary.
         """
-        for qudit in (self.drive_qubits + self.readout_resonators):
+        for qudit in self.qudits:
             assert isinstance(qudit, str), f'The type of {qudit} is not a string!'
             assert qudit.startswith('Q') or qudit.startswith('R'), f'The value of {qudit} is invalid.'
-        
+            
         for qubit in self.drive_qubits:
             if f'R{qubit[1:]}' not in self.readout_qubits: print(f'The {qubit} will not be readout!')
         
@@ -112,40 +107,37 @@ class Scan:
         assert isinstance(self.postpulse, dict), 'Postpulse must to be dictionary like {"Q0":[pulse1, pulse2,...]}'
 
 
-        
+    def generate_prepulse_postpulse_dataframe(self):
+        """
+        Generate the Pandas DataFrame of prepulse, postpulse, with padded 'I'.
+        Both subspace and input prepulse will be included into prepulse.
+        All qubits and resonators will become the (row) index of dataframe.
+        """
+
+        for qudit in self.qudits:
+            if qudit not in self.prepulse: self.prepulse[qudit] = []
+            if qudit not in self.postpulse: self.postpulse[qudit] = []
+
+        self.subspace_pulse = {}
+        for q, ss in zip(self.drive_qubits, self.subspace):
+            self.subspace_pulse[q] = [f'{q}/X180_{l}{l+1}' for l in range(int(ss[0]))]
+        for r in self.readout_resonators:
+            self.subspace_pulse[r] = []
 
 
+        # Generate the DataFrame
+        subspace_df = pd.DataFrame.from_dict(self.subspace_pulse, orient='index')
+        self.subspace_df = subspace_df.rename(columns={i:f'subspace_{i}' for i in range(subspace_df.shape[1])})
 
+        prepulse_df = pd.DataFrame.from_dict(self.prepulse, orient='index')
+        self.prepulse_df = prepulse_df.rename(columns={i:f'prepulse_{i}' for i in range(prepulse_df.shape[1])})
 
+        full_prepulse_df = pd.concat([subspace_df, prepulse_df], axis=1)
+        self.full_prepulse_df = full_prepulse_df.fillna('I')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        postpulse_df = pd.DataFrame.from_dict(self.postpulse, orient='index')
+        postpulse_df = postpulse_df.rename(columns={i:f'postpulse_{i}' for i in range(postpulse_df.shape[1])})
+        self.postpulse_df = postpulse_df.fillna('I')
 
 
     def make_sequence(self):
@@ -168,139 +160,161 @@ class Scan:
         Please check the link below for detail:
         https://qblox-qblox-instruments.readthedocs-hosted.com/en/master/tutorials/basic_sequencing.html
         """
-        self.sequences = {}        
+        self.sequences = {qudit:{} for qudit in self.qudits}        
         
-        for i, qudit in enumerate(self.drive_qubits + self.readout_resonators):
-            self.sequences[qudit] = {}
-            
-            
-            self.set_waveforms(qudit=qudit)
-            self.set_weights(qudit=qudit)
-            self.set_acquisitions(qudit=qudit)
-            self.set_program(qudit=qudit)
+        self.set_waveforms_acquisitions()
+        self.init_program()
+    
         
-        
-    def set_waveforms(self, qudit: str):
+    def set_waveforms_acquisitions(self):
         """
-        Generate waveforms items in self.sequences[qudit].
-        The input can be either 'Q#' or 'R#'.
-        """
-        waveforms = {qudit: {'data': get_waveform(self.config.varman[f'{qudit}/pulse_length'], 
-                                                  self.config.varman[f'{qudit}/pulse_shape']), 
-                             'index': 0}}
+        Generate waveforms, weights, acquisitions items in self.sequences[qudit].
         
-        self.sequences[qudit]['waveforms'] = waveforms
-        
-        
-    def set_weights(self, qudit: str):
-        """
-        Generate weights items in self.sequences[qudit].
-        The input can be either 'Q#' or 'R#'.
-        
-        It's about weights acquisition, and I believe even if we need it,
+        We skip weights acquisition, and I believe even if we need it,
         we can do it in post process. --Zihao(01/31/2023)
         Please check the link below for detail:
         https://qblox-qblox-instruments.readthedocs-hosted.com/en/master/tutorials/binned_acquisition.html
         """
-        self.sequences[qudit]['weights'] = {}       
+        for qudit in self.qudits:
+            pulse_type = 'qubit' if qudit.startswith('Q') else 'resonator'
+            waveforms = {qudit: {'data': get_waveform(self.config.varman[f'common/{pulse_type}_pulse_length'], 
+                                                      self.config.varman[f'{qudit}/pulse_shape']), 
+                                 'index': 0}}
+            acquisitions = {'readout':   {'num_bins': self.npoints, 'index': 0},
+                            'heralding': {'num_bins': self.npoints, 'index': 1}}
+            
+            self.sequences[qudit]['waveforms'] = waveforms
+            self.sequences[qudit]['weights'] = {}
+            self.sequences[qudit]['acquisitions'] = acquisitions           
 
-
-    def set_acquisitions(self, qudit: str):
-        """
-        Generate acquisitions items in self.sequences[qudit].
-        The input can be either 'Q#' or 'R#'.
-        """
-        acquisitions = {'readout':   {'num_bins': self.npoints,
-                                      'index': 0},
-                        'heralding': {'num_bins': self.npoints,
-                                      'index': 1}}
-        
-        self.sequences[qudit]['acquisitions'] = acquisitions
-        
-        
-    # TODO: Finish it.
-    def set_program(self, qudit: str):
-        """
-        Generate Q1ASM program items in self.sequences[qudit].
-        The input can be either 'Q#' or 'R#'.
-        """
-        
-        program = self.init_program()
-        program = self.add_relaxation(program)
-        program = self.add_heralding(qudit, program)
-        program = self.add_subspace_prepulse(qudit, program)
-        program = self.add_prepulse(qudit, program)
-        program = self.add_mainpulse(qudit, program)
-        program = self.add_postpulse(qudit, program)
-        program = self.add_readout(qudit, program)
-        program = self.add_stop(program)
-        
-        self.sequences[qudit]['program'] = program
-        
         
     def init_program(self):
-        program = """
-        # R0 is the main parameter of 1D Scan.
-        # R1 is the count of repetition.
-        # R2 is the relaxation time in microseconds.
         
-                    wait_sync        4
-                    move             0,R0
-                    move             0,R1
-                    move             0,R2
+        for qudit in self.qudits:
+            program = """
+            # R0 is the value of main parameter of 1D Scan.
+            # R1 is the count of repetition for algorithm or npoints for parameter sweep.
+            # R2 is the relaxation time in microseconds.
+            # Other register for backup.
+            
+                        wait_sync        4
+                        move             0,R0
+                        move             0,R1
+                        move             0,R2
+                        move             0,R3
+                        move             0,R4
+                        move             0,R5
+            
+            main_loop:  wait_sync        4                               # Sync at beginning of the loop.
+                        reset_ph                                         # Reset phase to eliminate effect of previous VZ gate.
+                        set_mrk          15                              # Enable all markers (binary 1111) for switching on output.
+                        upd_param        4                               # Update parameters and wait 4ns.
+            """
+            self.sequences[qudit]['program'] = program
         
-        main_loop:  wait_sync        4                               # Sync at beginning of the loop  
-        """
-        return program
         
+    def add_relaxation(self):
         
-    def add_relaxation(self, program: str):
         relaxation_time_s = self.config.varman['commmon/relaxation_time']
         relaxation_time_us = int( np.ceil(relaxation_time_s*1e6) )
         relaxation = f"""
+        #-----------Relaxation-----------
                     move             {relaxation_time_us},R2
         relx_loop:  wait             1000
                     loop             R2,@relx_loop
         """
-        return program + relaxation
+        for qudit in self.qudits: self.sequences[qudit]['program'] += relaxation
         
         
     # TODO: write it.
-    def add_heralding(self, qudit: str, program: str):
-        return program
+    def add_heralding(self):
+        return 
     
     
-    # TODO: Just take a try, don't be afraid.
-    def add_subspace_prepulse(self, qudit: str, program: str):
-        qubit_ss_prepulse = """
+    def add_prepulse(self):
         
-        """
-
-        resonator_ss_prepulse = """
-        
-        """       
+        for qudit in self.qudits:
+            prepulse = """
+            #-----------Prepulse-----------
+            """
+            init_pulse_str = self.full_prepulse_df.loc[qudit, :]  # Pandas Series, each element is like 'Q3/X180_01'
+            prepulse += pulse_interpreter(init_pulse_str)  # Long String, part of the sequence program.
             
-        subspace_prepulse = qubit_ss_prepulse if qudit.startswith('Q') else resonator_ss_prepulse
+            self.sequences[qudit]['program'] += prepulse
         
-        return program + subspace_prepulse
+
+    def add_mainpulse(self):        
+        """
+        Suppose to be called by child class.
+        """
+        print('The base experiment class has been called. No main pulse will be added.')
+        return
+
         
+    def add_postpulse(self):
         
+        for qudit in self.qudits:
+            postpulse = """
+            #-----------postpulse-----------
+            """
+            init_pulse_str = self.postpulse_df.loc[qudit, :]
+            postpulse += pulse_interpreter(init_pulse_str)
+            
+            self.sequences[qudit]['program'] += postpulse
+
+
+    # TODO: Maybe make RO as a pulse in pulse_interpreter.
+    def add_readout(self):
         
+        readout_length_s = self.config.varman['commmon/resonator_pulse_length']
+        readout_length_ns = int( np.ceil(readout_length_s*1e9) )
+        tof_s = self.config.varman['commmon/tof']
+        tof_ns = int( np.ceil(tof_s*1e9) )
         
+        for qubit in self.drive_qubits:
+            readout = f"""
+            #-----------readout-----------
+                        wait             {tof_ns + readout_length_ns} 
+            """
+            self.sequences[qubit]['program'] += readout
+            
+        for resonator in self.readout_resonators:
+            readout = f"""
+            #-----------readout-----------
+                        play             0,0,{tof_ns} 
+                        acquire          0,R1,{readout_length_ns}
+                        
+            """
+            self.sequences[resonator]['program'] += readout 
+            
+
+    def add_stop(self):
         
+        stop = f"""
+        #-----------stop-----------
+                    add              R1,1,R1
+                    set_mrk          0                               # Disable all markers (binary 0000) for switching off output.
+                    upd_param        4                               # Update parameters and wait 4ns.
+                    jlt              R1,{self.npoints},@main_loop
+                    
+                    stop             
+        """
+        for qudit in self.qudits: self.sequences[qudit]['program'] += stop
+
+
+    # TODO: Set a maximum of it.
+    def add_wait(self, time: int):
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        wait = f"""
+        #-----------wait-----------
+                    wait             {time}                               
+        """
+        for qudit in self.qudits: self.sequences[qudit]['program'] += wait
+    
+            
+    # TODO: write it.        
+    def add_pulse(self, pulse: dict):
+        pass
         
         
     @staticmethod
