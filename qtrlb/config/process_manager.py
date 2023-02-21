4f66a365d784ce85999e6165f9f1d83ea03ee360
Emrys <zhwang000@gmail.com>
2023-02-21 05:24:29 -0500

    Finish heralding and mask

--- qtrlb/config/process_manager.py
+++ qtrlb/config/process_manager.py
@@ -66,12 +66,14 @@ class ProcessManager(Config):
         Because one may need to keep heralding to be true to add that pulse in sequence,
         while going into the customized process routine.
         """
-        # r is 'R3', 'R4', data_dict has key 'Heterodyned_readout', etc.
-        for r, data_dict in self.measurement.items():   
-            if self['customized']:
-                continue
-            
-            elif self['heralding']:
+
+        if self['customized']:
+            pass
+        
+        
+        elif self['heralding']:
+            # r is 'R3', 'R4', data_dict has key 'Heterodyned_readout', etc.
+            for r, data_dict in self.measurement.items():  
                 data_dict['IQrotated_readout'] = rotate_IQ(data_dict['Heterodyned_readout'], 
                                                            angle=self[f'{r}/IQ_rotation_angle'])
                 data_dict['IQrotated_heralding'] = rotate_IQ(data_dict['Heterodyned_heralding'], 
@@ -84,8 +86,23 @@ class ProcessManager(Config):
                                                                   means=self[f'{r}/IQ_means'], 
                                                                   covariances=self[f'{r}/IQ_covariances'])
                 
+            heralding_mask = self.heralding_test()
+            
+            for r, data_dict in self.measurement.items(): 
+                data_dict['Mask_heralding'] = heralding_mask
                 
-            elif self['classification']:
+                data_dict['PopulationNormalized_readout'] = normalize_population(data_dict['GMMpredicted_readout'],
+                                                                                 n_levels=self[f'{r}/n_readout_levels'],
+                                                                                 mask=data_dict['Mask_heralding'])
+                
+                data_dict['PopulationCorrected_readout'] = np.linalg.solve(self[f'{r}/corr_matrix'],
+                                                                           data_dict['PopulationNormalized_readout'])
+                
+                data_dict['to_fit'] = data_dict['PopulationCorrected_readout']
+            
+            
+        elif self['classification']:
+            for r, data_dict in self.measurement.items():  
                 data_dict['IQrotated_readout'] = rotate_IQ(data_dict['Heterodyned_readout'], 
                                                            angle=self[f'{r}/IQ_rotation_angle'])
                 
@@ -100,23 +117,57 @@ class ProcessManager(Config):
                                                                            data_dict['PopulationNormalized_readout'])
                 
                 data_dict['to_fit'] = data_dict['PopulationCorrected_readout']
-                # TODO: think about how we do fit. Need to consider 2D data and multiple qubit scan.
-                # Maybe it really worth to have the fit and plot outside the process_data.
-                # Especially we actually have multiple level to fit.
-                
-            else:
+
+            
+        else:
+            for r, data_dict in self.measurement.items():  
                 data_dict['IQrotated_readout'] = rotate_IQ(data_dict['Heterodyned_readout'], 
                                                            angle=self[f'{r}/IQ_rotation_angle'])
-
+    
                 data_dict['IQautorotated_readout'] = autorotate_IQ(data_dict['IQrotated_readout'], 
                                                                    n_components=self[f'{r}/n_readout_levels'])
-
+    
                 data_dict['IQaveraged_readout'] = np.mean(data_dict['IQautorotated_readout'], axis=1)
         
-                data_dict['to_fit'] = data_dict['IQaveraged_readout'][0]
+                data_dict['to_fit'] = data_dict['IQaveraged_readout']
         
 
+    def heralding_test(self):
+        """
+        Generate the ndarray heralding_mask with shape (n_reps, x_points).
+        The entries will be 0 only if all resonators gives 0 in heralding.
+        It means for that specific repetition and x_point, all resonators pass heralding test.
+        We then truncate data to make sure all x_point has same amount of available repetition.
+        
+        Note from Zihao(02/21/2023):
+        The code here is stolen from original version of qtrl where we can only test ground state.
+        However, ground state has most population and if our experiment need to start from |1>, pi pulse it.
+        The code here is ugly and hard to read, please make it better if you know how to do it.
+        """
+        resonators = self.measurement.keys()
+        heralding_mask = np.zeros_like(self.measurement[resonators[0]]['GMMpredicted_heralding'])
+        
+        for r in resonators:
+            heralding_mask = heralding_mask | self.measurement[r]['GMMpredicted_heralding']
+            
+        n_pass_min = np.min(np.sum(heralding_mask == 0, axis=0))  
+        
+        for i in range(heralding_mask.shape[1]): # Loop over each x_point
+            j = 0
+            while np.sum(heralding_mask[:, i] == 0) > n_pass_min:
+                n_short = np.sum(heralding_mask[:, i] == 0) - n_pass_min
+                heralding_mask[j : j + n_short, i] = -1
+                j += n_short
+                
+        return heralding_mask 
 
-    def fit_data(self): # Think about which layer to put it will be better.
-        pass
+
+    def fit_data(self): 
+        # TODO: think about how we do fit. Need to consider 2D data and multiple qubit scan.
+        # Maybe it really worth to have the fit and plot outside the process_data.
+        # Especially we actually have multiple level to fit.
+        # Think about which layer to put it will be better.
+        # Think about the two level trick of fit.
+        # data_dict['to_fit'] usually have shape (n_levels, x_points), or (2, x_points) for IQ.
+        return
     
