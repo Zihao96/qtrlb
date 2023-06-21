Fix lowest level in  gmm_predict and normalization

qtrlb/calibration/calibration.py
@@ -119,6 +119,11 @@ class Scan:
         Note from Zihao(03/03/2023):
         We call implement_parameters methods here instead of during init/load of DACManager, \
         because we want those modules/sequencers not being used to keep their default status.
+
+        Note from Zihao(06/21/2023):
+        I didn't use ** on these kwargs. I believe dict bring us better encapsulation.
+        Pass in a whole dictionary can be more general than just pass 'something = somevalue'.
+        It's because the key doen't need to be string anymore.
         """
         self.experiment_suffix = experiment_suffix
         self.n_pyloops = n_pyloops
@@ -135,10 +140,10 @@ class Scan:
         self.make_exp_dir()  # It also save a copy of yamls and jsons there.
         self.acquire_data()  # This is really run the thing and return to the IQ data in self.measurement.
         self.cfg.data.save_measurement(self.data_path, self.measurement, self.attrs)
-        self.process_data(**self.process_kwargs)
-        self.fit_data(**self.fitting_kwargs)
+        self.process_data()
+        self.fit_data()
         self.cfg.data.save_measurement(self.data_path, self.measurement, self.attrs)
-        self.plot(**self.plot_kwargs)
+        self.plot()
         self.n_runs += 1
         self.measurements.append(self.measurement)
         
@@ -636,7 +641,7 @@ class Scan:
             print(f'Scan: Pyloop {i} finished!')
             
             
-    def process_data(self, **process_kwargs):
+    def process_data(self):
         """
         Process the data by performing reshape, rotation, average, GMM, fit, plot, etc.
         We keep this layer here to provide possibility to inject functionality between acquire and fit.
@@ -644,7 +649,7 @@ class Scan:
         """
         self.cfg.process.process_data(self.measurement, 
                                       shape=(2, self.n_reps, self.x_points),
-                                      **process_kwargs)
+                                      process_kwargs=self.process_kwargs)
         
 
     def fit_data(self, x: list | np.ndarray = None, **fitting_kwargs): 
@@ -1003,14 +1008,14 @@ class Scan2D(Scan):
         for tone in self.tones: self.sequences[tone]['program'] += y_end
 
 
-    def process_data(self, **process_kwargs):
+    def process_data(self):
         """
         Process the data by performing reshape, rotation, average, GMM, fit, plot, etc.
         We keep this layer here to provide possibility to inject functionality between acquire and fit.
         """
         self.cfg.process.process_data(self.measurement, 
                                       shape=(2, self.n_reps, self.y_points, self.x_points),
-                                      **process_kwargs)
+                                      process_kwargs=self.process_kwargs)
 
 
     def plot(self):

qtrlb/config/process_manager.py
@@ -1,7 +1,7 @@
 import numpy as np
 from qtrlb.config.config import Config
 from qtrlb.config.variable_manager import VariableManager
-from qtrlb.processing.processing import rotate_IQ, gmm_predict, normalize_population, autorotate_IQ, correct_population
+from qtrlb.processing.processing import rotate_IQ, gmm_predict, normalize_population, autorotate_IQ, correct_population, two_tone_predict
 
 
 class ProcessManager(Config):
@@ -52,7 +52,7 @@ class ProcessManager(Config):
                 self[f'{r}/IQ_means'] = [[i*10, i*10] for i in range(self[f'{r}/n_readout_levels'])]
                 
                 
-    def process_data(self, measurement: dict, shape: tuple, **process_kwargs):
+    def process_data(self, measurement: dict, shape: tuple, process_kwargs: dict = None):
         """
         Process the data by performing reshape, rotation, average, GMM, fit, plot, etc.
         Three common routine are hard coded here since we never change them.
@@ -67,7 +67,7 @@ class ProcessManager(Config):
         """
 
         if self['customized'] is not None:
-            getattr(self, self['customized'])(measurement, shape, **process_kwargs)
+            getattr(self, self['customized'])(measurement, shape, process_kwargs)
         
         
         elif self['heralding']:
@@ -81,12 +81,15 @@ class ProcessManager(Config):
                 data_dict['IQrotated_heralding'] = rotate_IQ(data_dict['Reshaped_heralding'], 
                                                              angle=self[f'{r}/IQ_rotation_angle'])
                 
-                data_dict['GMMpredicted_readout'] = gmm_predict(data_dict['IQrotated_readout'], 
-                                                                means=self[f'{r}/IQ_means'], 
-                                                                covariances=self[f'{r}/IQ_covariances'])
-                data_dict['GMMpredicted_heralding'] = gmm_predict(data_dict['IQrotated_heralding'], 
-                                                                  means=self[f'{r}/IQ_means'], 
-                                                                  covariances=self[f'{r}/IQ_covariances'])
+                data_dict['GMMpredicted_readout'] = (gmm_predict(data_dict['IQrotated_readout'], 
+                                                                 means=self[f'{r}/IQ_means'], 
+                                                                 covariances=self[f'{r}/IQ_covariances'])
+                                                     + self[f'{r}/readout_levels'][0])
+                
+                data_dict['GMMpredicted_heralding'] = (gmm_predict(data_dict['IQrotated_heralding'], 
+                                                                   means=self[f'{r}/IQ_means'], 
+                                                                   covariances=self[f'{r}/IQ_covariances'])
+                                                       + self[f'{r}/readout_levels'][0])
                 
             heralding_mask = self.heralding_test(measurement=measurement)
             
@@ -94,7 +97,7 @@ class ProcessManager(Config):
                 data_dict['Mask_heralding'] = heralding_mask  # So that it can be save to hdf5.
                 
                 data_dict['PopulationNormalized_readout'] = normalize_population(data_dict['GMMpredicted_readout'],
-                                                                                 n_levels=self[f'{r}/n_readout_levels'],
+                                                                                 levels=self[f'{r}/readout_levels'],
                                                                                  mask=heralding_mask)
                 
                 data_dict['PopulationCorrected_readout'] = correct_population(data_dict['PopulationNormalized_readout'],
@@ -110,12 +113,13 @@ class ProcessManager(Config):
                 data_dict['IQrotated_readout'] = rotate_IQ(data_dict['Reshaped_readout'], 
                                                            angle=self[f'{r}/IQ_rotation_angle'])
                 
-                data_dict['GMMpredicted_readout'] = gmm_predict(data_dict['IQrotated_readout'], 
-                                                                means=self[f'{r}/IQ_means'], 
-                                                                covariances=self[f'{r}/IQ_covariances'])
+                data_dict['GMMpredicted_readout'] = (gmm_predict(data_dict['IQrotated_readout'], 
+                                                                 means=self[f'{r}/IQ_means'], 
+                                                                 covariances=self[f'{r}/IQ_covariances'])
+                                                     + self[f'{r}/readout_levels'][0])
                 
                 data_dict['PopulationNormalized_readout'] = normalize_population(data_dict['GMMpredicted_readout'],
-                                                                                 n_levels=self[f'{r}/n_readout_levels'])
+                                                                                 levels=self[f'{r}/readout_levels'])
                 
                 data_dict['PopulationCorrected_readout'] = correct_population(data_dict['PopulationNormalized_readout'],
                                                                               self[f'{r}/corr_matrix'])
@@ -174,5 +178,58 @@ class ProcessManager(Config):
     ##################################################           
     # All functions below are different customized data processing.
     # Use them by change customized_data_process in variables.yaml
-    def two_tone_readout(self, measurement: dict, shape: tuple, **process_kwargs):
-        pass
+    def two_tone_readout(self, measurement: dict, shape: tuple, process_kwargs: dict):
+        """
+        Using two tones frequency-multiplexing to readout one resonator.
+        Generate exactly same 'to_fit' value for both tones to avoid later bug.
+        
+        Example of process_kwargs:
+        {
+            ('R0', 'R1'): corr_matrix,
+            ('R2', 'R3'): corr_matrix,
+        }
+
+        Note from Zihao(2023/06/21):
+        I know it won't be elegant, but I also don't want to do premature optimization.
+        Let's make it better when we really need to. 
+        I didn't use ** on these kwargs. I believe dict bring us better encapsulation.
+        Pass in a whole dictionary can be more general than just pass 'something = somevalue'.
+        It's because the key doen't need to be string anymore.
+        """
+        # Normal GMM prediction as classification.
+        for r, data_dict in measurement.items():  
+            data_dict['Reshaped_readout'] = np.array(data_dict['Heterodyned_readout']).reshape(shape)
+            
+            data_dict['IQrotated_readout'] = rotate_IQ(data_dict['Reshaped_readout'], 
+                                                        angle=self[f'{r}/IQ_rotation_angle'])
+            
+            data_dict['GMMpredicted_readout'] = (gmm_predict(data_dict['IQrotated_readout'], 
+                                                             means=self[f'{r}/IQ_means'], 
+                                                             covariances=self[f'{r}/IQ_covariances'])
+                                                 + self[f'{r}/readout_levels'][0])
+            # GMMpredicted has shape (n_reps, y_points, x_points) for 2D Scan.
+            # Values are integers as state assignment result.
+
+
+        for (tone_0, tone_1), corr_matrix in process_kwargs.items():
+            twotonepredicted_readout, mask_twotone = two_tone_predict(measurement[tone_0]['GMMpredicted_readout'],
+                                                                      measurement[tone_1]['GMMpredicted_readout'],
+                                                                      self[f'{tone_0}/readout_levels'],
+                                                                      self[f'{tone_1}/readout_levels'])
+            population_normalized_readout = normalize_population(twotonepredicted_readout,
+                                                                 levels=np.union1d(self[f'{tone_0}/readout_levels'],
+                                                                                   self[f'{tone_1}/readout_levels']),
+                                                                 mask=mask_twotone)
+            population_corrected_readout = correct_population(population_normalized_readout, corr_matrix)
+
+            measurement[tone_0]['Mask_twotone'] = mask_twotone
+            measurement[tone_1]['Mask_twotone'] = mask_twotone
+            measurement[tone_0]['TwoTonepredicted_readout'] = twotonepredicted_readout
+            measurement[tone_1]['TwoTonepredicted_readout'] = twotonepredicted_readout
+            measurement[tone_0]['PopulationNormalized_readout'] = population_normalized_readout
+            measurement[tone_1]['PopulationNormalized_readout'] = population_normalized_readout
+            measurement[tone_0]['PopulationCorrected_readout'] = population_corrected_readout
+            measurement[tone_1]['PopulationCorrected_readout'] = population_corrected_readout
+            measurement[tone_0]['to_fit'] = population_corrected_readout
+            measurement[tone_1]['to_fit'] = population_corrected_readout
+            

qtrlb/processing/processing.py
@@ -53,6 +53,7 @@ def gmm_predict(input_data, means, covariances, covariance_type='spherical'):
     By default, means should have shape (n_components, 2) for 2D gaussian.
     Covariances should have shape (n_components,) for symmetrical distribution,
     where n_components is the number of Gaussian blob in IQ plane.
+    The return values are always count from zero.
     
     Reference:
     https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html
@@ -86,9 +87,9 @@ def gmm_fit(input_data, n_components: int, covariance_type='spherical'):
     return gmm.means_, gmm.covariances_
 
 
-def normalize_population(input_data, n_levels: int, axis: int = 0, mask: np.ndarray = None):
+def normalize_population(input_data, levels: list | np.ndarray, axis: int = 0, mask: np.ndarray = None):
     """
-    Count population (specific interger) for n_levels along a given axis.
+    Count population (specific interger) for different levels along a given axis.
     Return to normalized population (counts of appearing) with shape (n_levels, x_points).
     Allow a mask to pick entries in input_data to be normalized.
     Typically, the input_data and mask should have shape (n_reps, x_points).
@@ -103,7 +104,7 @@ def normalize_population(input_data, n_levels: int, axis: int = 0, mask: np.ndar
     """
     # Zihao(02/17/2023): It's short, but still worth a function with clear explanation.
     masked_data = np.ma.MaskedArray(input_data, mask=mask)
-    result = [np.mean(masked_data==level, axis=axis) for level in range(n_levels)]
+    result = [np.mean(masked_data==level, axis=axis) for level in levels]
     return np.array(result)
 
 
@@ -160,3 +161,50 @@ def get_readout_fidelity(confusion_matrix: list | np.ndarray) -> float:
     """
     fidelity = np.mean(np.diagonal(confusion_matrix))
     return float(fidelity)
+
+
+def two_tone_predict(input_data_0: list | np.ndarray, 
+                     input_data_1: list | np.ndarray, 
+                     levels_0: list | np.ndarray, 
+                     levels_1: list | np.ndarray) -> tuple(np.ndarray, np.ndarray):
+    """
+    Classify the qudit state based on result of GMM prediction from two tones.
+    Levels are list of possible state assignment result of corresponding tones.
+    We need one and only one element appearing in both levels list.
+    We will generate a mask to drop the data that has contradiction when normalizing it.
+    The result and mask should have same shape as two input data.
+    """
+    input_data_0 = np.array(input_data_0) 
+    input_data_1 = np.array(input_data_1)
+    levels_0 = np.array(levels_0)
+    levels_1 = np.array(levels_1)
+
+    # Find intersection and check it's unique.
+    intersection = np.intersect1d(levels_0, levels_1)
+    assert len(intersection) == 1, 'More than one state are reading out by both tone!'
+    intersection_array = intersection * np.ones(shape=input_data_0.shape)
+
+    # Element-wise comparision for finding the contradiction.
+    mask_boolean_0 = np.equal(intersection_array, input_data_0)
+    mask_boolean_1 = np.equal(intersection_array, input_data_0)
+    mask = 1 - (mask_boolean_0 | mask_boolean_1)  
+    # 1-True is zero and will be kept, 1-False is one and will be masked.
+
+
+    # Try logic first.
+    a = 0
+    b = 0
+
+    # Contradiction happen.
+    if a != levels_0[-1] and b != levels_1[0]:
+        result = np.nan
+
+    # Use a as result.
+    elif a != levels_0[-1]:
+        result = a
+
+    # Use b as result
+    else:
+        result = b
+
+    return result, mask