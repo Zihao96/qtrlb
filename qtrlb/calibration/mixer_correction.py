ef0b4d000420ee446148b07e9b4a54d5c05804e2
Emrys <zhwang000@gmail.com>
2023-08-19 14:20:30 -0400

    Add MixerAutoCorrection

--- qtrlb/calibration/mixer_correction.py
+++ qtrlb/calibration/mixer_correction.py
@@ -3,7 +3,11 @@ import json
 import time
 import numpy as np
 import ipywidgets as ipyw
+import qtrlb.utils.units as u
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 from qtrlb.config.config import MetaManager
+from qtrlb.utils.N9010A import N9010A
 
 
 
@@ -200,3 +204,192 @@ class MixerCorrection:
         self.sequencer.mixer_corr_phase_offset_degree(phase_offset)
         self.sequencer.arm_sequencer()
         self.cfg.DAC.qblox.start_sequencer()
+
+
+
+
+class MixerAutoCorrection(MixerCorrection):
+    """ Automate mixer correction by using Spectrum Analyzer N9010A.
+        The sb in code refer to the mirrored sideband in the signal.
+        It's not the one we want to keep.
+    
+        Attributes:
+            sa: A spectrum analyer object.
+            cfg: A MetaManager
+            qudit: 'Q2', 'Q3', 'R4', 'R1'
+            subspace: '01', '12'. Only work for qubit, not resonator.
+            amp: Float number between 0 and 1.
+            waveform_length: Integer between [4,16384]. Don't change.
+    """
+    def __init__(self, 
+                 sa: N9010A,
+                 cfg: MetaManager, 
+                 qudit: str,
+                 subspace: str = '01',
+                 amp: float = 0.1,
+                 waveform_length: int = 40):
+        
+        super().__init__(cfg, qudit, subspace, amp, waveform_length)
+        self.sa = sa
+
+        # Get the exact frequency for convenience.
+        lo_key = 'qubit_lo' if self.qudit.startswith('Q') else 'resonator_lo'
+        self.main_freq = self.cfg[f'variables.{self.tone}/freq']
+        self.lo_freq = self.cfg[f'variables.{self.qudit}/{lo_key}']
+        self.sb_freq = self.lo_freq * 2 - self.main_freq
+        self.mod_freq = self.main_freq - self.lo_freq
+
+
+    def run(self,
+            which: str = 'both',
+            save_cfg: bool = False,
+            method: str = 'Powell',
+            readout_delay_time: float = 0.02,
+            readout_avg_num: int = 5,
+            lo_maxiter: int = 3,
+            sb_maxiter: int = 2):
+        """
+        Run optimization on specified tones.
+
+        Parameters:
+        readout_delay_time: The time 
+        """
+        
+        self.minimize_method = method
+        self.readout_delay_time = readout_delay_time
+        self.readout_avg_num = readout_avg_num
+        self.lo_maxiter = lo_maxiter
+        self.sb_maxiter = sb_maxiter
+
+        # Start signal output and load current parameter.
+        self.make_sequence()
+        self.set_offset0(
+            self.cfg[f'DAC.Module{self.module_idx}/out{self.out}_offset_path0']
+        )
+        self.set_offset1(
+            self.cfg[f'DAC.Module{self.module_idx}/out{self.out}_offset_path1']
+        )
+        self.set_gain_ratio(
+            self.cfg[f'DAC.Module{self.module_idx}/Sequencer{self.sequencer_idx}/mixer_corr_gain_ratio']
+        )
+        self.set_phase_offset(
+            self.cfg[f'DAC.Module{self.module_idx}/Sequencer{self.sequencer_idx}/mixer_corr_phase_offset_degree']
+        )
+
+        # Start optimization.
+        self.set_sa()
+        self.old_spectrum = self.sa.data
+
+        if which.lower() == 'both':
+            self.minimize_lo()
+            self.minimize_sb()
+        elif which.lower() == 'lo':
+            self.minimize_lo()
+        elif which.lower() == 'sb':
+            self.minimize_sb()
+        else:
+            raise ValueError(f'MixerAutoCorrection: The "which" argument can only take "both", "lo", "sb".')
+
+        self.new_spectrum = self.sa.data
+        self.stop(save_cfg)
+        self.plot()
+
+
+    def set_sa(self):
+        """
+        Set up instrument before doing optimization.
+        """
+        # A wide span with all three peaks on screen.
+        self.sa.set('freq_center', self.lo_freq)
+        self.sa.set('freq_span', 2.4 * abs(self.mod_freq))
+        self.sa.set('res_bw', abs(self.mod_freq) * 1e-4)
+        self.sa.set('vid_bw_auto')
+        self.sa.set('ref_level', '15')
+
+        # Set three markers on three peaks.
+        self.sa.set_marker(1, 'ON')
+        self.sa.set_marker(2, 'ON')
+        self.sa.set_marker(3, 'ON')
+        self.sa.set_marker_center(1)
+        self.sa.set_marker_center(2)
+        self.sa.set_marker_center(3)
+
+        # Maddy leave the 0.1 second delay here and probably has her reason.
+        peak_left, peak_right = 3, 1 if self.mod_freq > 0 else 1, 3
+        time.sleep(0.1)
+        self.sa.set_marker('peak_left', peak_left)
+        time.sleep(0.1)
+        self.sa.set_marker('peak_right', peak_right)
+
+        # Set up the proper reference level
+        peak_highest = np.max([float(self.sa.get_marker('y', i+1)) for i in range(3)])
+        ref_level = np.round(peak_highest) + 5
+        self.sa.set('ref_level', ref_level)
+
+
+    def minimize_lo(self):
+        """
+        Minimize the LO tone using scipy.optimize.minimize.
+        Here we zoom into the LO tone such that there is only one wide peak on screen.
+        """
+        self.sa.set('freq_center', self.lo_freq)
+        self.sa.set('freq_span', '2MHz')
+        self.sa.set('res_bw', '10kHz')
+        result = minimize(self.loss_func_lo, x0=(0,0), method=self.minimize_method, 
+                          bounds=((-84,73), (-84,73)), options = {'maxiter' : self.lo_maxiter})
+        self.set_offset0(result.x[0])
+        self.set_offset1(result.x[1])
+
+
+    def loss_func_lo(self, x) -> float:
+        """
+        The function we pass into minimize for reduce LO tone.
+        Here x[0] will be offset0, x[1] will be offset1.
+        Here we read marker several times and take average to reduce fluctuation.
+        """
+        self.set_offset0(x[0])
+        self.set_offset1(x[1])
+        time.sleep(self.readout_delay_time)
+
+        return np.mean([float(self.sa.get_marker('y', 2)) for _ in range(self.readout_avg_num)])
+    
+
+    def minimize_sb(self):
+        """
+        Minimize the SB tone using scipy.optimize.minimize.
+        Here we zoom into the SB tone such that there is only one wide peak on screen.
+        We amplify the gain ratio (0.5-2.0) 100 times to avoid vanishing/exploding gradients.
+        """
+        self.sa.set('freq_center', self.sb_freq)
+        self.sa.set('freq_span', '2MHz')
+        self.sa.set('res_bw', '10kHz')
+        result = minimize(self.loss_func_sb, x0=(100, 0), method=self.minimize_method, 
+                          bounds=((50,200), (-45,45)), options={'maxiter': self.sb_maxiter})
+        self.set_gain_ratio(result.x[0]/100)
+        self.set_phase_offset(result.x[1])
+
+
+    def loss_func_sb(self, x) -> float:
+        """
+        The function we pass into minimize for reduce SB tone.
+        Here x[0] will be offset0, x[1] will be offset1.
+        Here we read marker several times and take average to reduce fluctuation.
+        """
+        self.set_gain_ratio(x[0]/100)
+        self.set_phase_offset(x[1])
+        time.sleep(self.readout_delay_time)
+
+        return np.mean([float(self.sa.get_marker('y', 3)) for _ in range(self.readout_avg_num)])
+    
+
+    def plot(self):
+        """
+        Plot the spectrum before/after mixer correction for user to compare them.
+        """
+        fig, ax = plt.subplots(2, 1, figsize=(8,8))
+        ax[0].plot(self.old_spectrum[0]/u.GHz, self.old_spectrum[1], alpha=0.8)
+        ax[1].plot(self.new_spectrum[0]/u.GHz, self.new_spectrum[1], alpha=0.8)
+        ax[0].set(xlabel='Frequency [GHz]', ylabel='Power [dBm]', title='Before optimization')
+        ax[1].set(xlabel='Frequency [GHz]', ylabel='Power [dBm]', title='After optimization')
+        self.fig = fig
+
---
+++ qtrlb/utils/N9010A.py
@@ -0,0 +1,102 @@
+"""
+This file was originally written by Maddy Ramsey when she was a REU student in BlokLab in 2023 summer.
+I rewrite it later but keep her idea and main effort on construct SCPI command.
+"""
+
+
+import pyvisa
+import numpy as np
+from typing import Any
+
+
+class N9010A:
+    """ Python driver for Keysight EXA Signal Analyzer N9010A.
+        It's designed to automate mixer correction.
+    """
+
+    command_dict = { 
+        'freq_span': 'FREQuency:SPAN',
+        'freq_span_prev': 'FREQuency:SPAN:PREVious',
+        'freq_center': 'FREQuency:CENTer',
+        'res_bw': 'BANDwidth:RESolution',
+        'vid_bw': 'BANDwidth:VID',
+        'vid_bw_auto': 'BAND:VID:AUTO',
+        'peak_list': 'CALC:DATA:PEAK',
+        'data': 'CALCulate:DATA',
+        'ref_level': ':DISPlay:WINDow:TRACe:Y:RLEVel',
+        'scale/div': ':DISPlay:WINDow:TRACe:Y:PDIVision',
+        'excursion': ':CALCulate:MARKer:PEAK:EXCursion',
+        'excursion_status': ':CALCulate:MARKer:PEAK:EXCursion:STATe',
+        'threshold': ':CALCulate:MARKer:PEAK:THReshold',
+        'threshold_status': ':CALCulate:MARKer:PEAK:THReshold:STATe',
+        'sweep_points': ':SWEep:POINts'
+    }    
+
+    marker_command_dict = {
+        'max_peak': ':MAXimum',
+        'next_peak': 'MAXimum:NEXT',
+        'peak_right': ':MAXimum:RIGHt',
+        'peak_left': ':MAXimum:LEFT',
+        'x': ':X',
+        'y': ':Y',
+        'freq': ':X',
+        'ON': ':MODE POS',
+        'OFF': ':MODE OFF',
+        'all_off': ':AOFF'
+    }
+
+    def __init__(self, ip_address: str = '192.168.1.14'):
+        self.ip_address = ip_address
+        self.resource_name = f'TCPIP0::{self.ip_address}::INSTR'
+        self.inst = pyvisa.ResourceManager().open_resource(self.resource_name)
+
+
+    def get(self, key: str, *args: tuple[str]) -> str | np.ndarray:
+        """
+        Get the value of the given setting parameter (key) from instrument.
+        Normally it will return to the str, unless we try to get data.
+        """
+        message = ' '.join([f'{self.command_dict[key]}?', *args])
+        return self.inst.query(message)
+
+
+    def set(self, key: str, value: Any = '', *args: tuple[str]) -> None:
+        """
+        Set the value of the given setting parameter (key) to instrument.
+        Normally it will return to the str, unless we try to get data.
+        """
+        message = ' '.join([f'{self.command_dict[key]}', str(value), *args])
+        self.inst.write(message)
+
+
+    def get_marker(self, marker: int | str, key: str, *args: tuple[str]) -> str:
+        """
+        Get the value of the given marker parameter (key) from instrument.
+        """
+        message = ' '.join([f'CALCulate:MARKer{marker}{self.marker_command_dict[key]}?', *args])
+        return self.inst.query(message)
+
+
+    def set_marker(self, marker: int | str, key: str, value: Any = '', *args: tuple[str]) -> None:
+        """
+        Set the value of the given marker parameter (key) from instrument.
+        """
+        message = ' '.join([f'CALCulate:MARKer{marker}{self.marker_command_dict[key]}', str(value), *args])
+        self.inst.write(message)
+    
+
+    def set_marker_center(self, marker: int | str):
+        """
+        Convenient function to set a given maker to the center frequency.
+        """
+        center_freq = self.get('freq_center')
+        self.set_marker(marker, 'x', value=center_freq)
+
+
+    @property
+    def data(self):
+        data_str = self.get('data')
+        val_array = np.genfromtxt([data_str], delimiter=",")
+        x_vals, y_vals = val_array[0::2], val_array[1::2]
+        return x_vals, y_vals
+
