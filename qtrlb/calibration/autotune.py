55998bb3fbf5c0cc20a3b8e489c148e319b682df
Zihao Wang <zhwang000@gmail.com>
2025-07-21 11:10:34 -0400

    Return the calibration even if not updating parameters

--- qtrlb/calibration/autotune.py
+++ qtrlb/calibration/autotune.py
@@ -30,7 +30,8 @@ def autotune(
     Change hardware configuration and cooldown/warmup fridge won't be considered here.
     autotune_kwargs take arguments common to all four scans here, such as pre_gate/main_tones
     """
-    assert show_plot >= require_confirmation, 'Autotune: show_plot must be True if require_confirmation'
+    if show_plot < require_confirmation:
+        raise ValueError('Autotune: show_plot must be True if require_confirmation')
 
     if 'main_tones' in autotune_kwargs:
         main_tones = make_it_list(autotune_kwargs['main_tones'])
@@ -44,20 +45,13 @@ def autotune(
     amp_180 = cfg[f'variables.{main_tone}/amp_180']
     weight = cfg[f'variables.{main_tone}/DRAG_weight']
 
-    # Instantiate the classes.
+    # Run Ramsey
     ramsp = RamseyScan(cfg, drive_qubits, readout_tones, subspace=subspace, 
                        length_start=0, length_stop=rams_length, length_points=41, 
                        artificial_detuning=+rams_AD, level_to_fit=level_to_fit, **autotune_kwargs)
-
     ramsn = RamseyScan(cfg, drive_qubits, readout_tones, subspace=subspace, 
                        length_start=0, length_stop=rams_length, length_points=41, 
                        artificial_detuning=-rams_AD, level_to_fit=level_to_fit, **autotune_kwargs)
-
-    das = DriveAmplitudeScan(cfg, drive_qubits, readout_tones, subspace=subspace, 
-                             amp_start=amp_180*0.85/0.9, amp_stop=amp_180*0.95/0.9, amp_points=41, 
-                             error_amplification_factor=9, fitmodel=QuadModel, 
-                             level_to_fit=level_to_fit, **autotune_kwargs)
-    # Run Ramsey
     ramsp.run(f'AD+{round(rams_AD/u.kHz)}kHz_autotune')
     ramsn.run(f'AD-{round(rams_AD/u.kHz)}kHz_autotune')
 
@@ -72,7 +66,7 @@ def autotune(
     if require_confirmation:
         plt.show()
         confirmation = input('Autotune: press 1 to update parameter, any others to cancel.').startswith('1')
-        if not confirmation: return
+        if not confirmation: return ramsp, ramsn, None, None
 
     # Update parameters.
     freq_p = ramsp.fit_result[rr].params['freq'].value
@@ -82,13 +76,17 @@ def autotune(
     cfg.load()  # Load will help to generate correct mod_freq.
 
     # Run DAS
+    das = DriveAmplitudeScan(cfg, drive_qubits, readout_tones, subspace=subspace, 
+                             amp_start=amp_180*0.85/0.9, amp_stop=amp_180*0.95/0.9, amp_points=41, 
+                             error_amplification_factor=9, fitmodel=QuadModel, 
+                             level_to_fit=level_to_fit, **autotune_kwargs)
     das.run('EAx9_autotune')
 
     # Check whether we should go on or stop
     if require_confirmation:
         plt.show()
         confirmation = input('Autotune: press 1 to update parameter, any others to cancel.').startswith('1')
-        if not confirmation: return
+        if not confirmation: return ramsp, ramsn, das, None
 
     # Update parameters.
     cfg[f'variables.{main_tone}/amp_180'] = das.fit_result[rr].params['x0'].value
@@ -114,7 +112,7 @@ def autotune(
     if require_confirmation:
         plt.show()
         confirmation = input('Autotune: press 1 to update parameter, any others to cancel.').startswith('1')
-        if not confirmation: return
+        if not confirmation: return ramsp, ramsn, das, dws
 
     # Update parameters.
     cfg[f'variables.{main_tone}/DRAG_weight'] = dws.fit_result[rr].params['x0'].value
